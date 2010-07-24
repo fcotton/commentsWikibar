03f763dfb81f7e48c44d8214cab0917d3bf96d6c
Turbinagaz.com <github@turbinagaz.com>
2010-07-24 14:43:07 +0200

    Forgot some stuff...

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
 	/* Name */		"Comments Wikibar",
 	/* Description*/	"Adds a formatting toolbar when public comments use the wiki syntax",
 	/* Author */		"Pep and contributors",
-	/* Version */		'1.1-beta1',
+	/* Version */		'1.1-beta2',
 	/* Permissions */	'contentadmin'
 );
 ?>
--- _public.php
+++ _public.php
@@ -10,68 +10,85 @@
 # -- END LICENSE BLOCK ------------------------------------
 if (!defined('DC_RC_PATH')) return;
 
-$core->addBehavior('publicHeadContent',array('commentsWikibarBehaviors','publicHeadContent'));
+$core->addBehavior('publicHeadContent',  array('commentsWikibarBehaviors','publicHeadContent'));
+$core->addBehavior('publicFooterContent',array('commentsWikibarBehaviors','publicFooterContent'));
 
 class commentsWikibarBehaviors
 {
+	protected static function canActivate()
+	{
+		global $core;
+		
+		if ( $core->blog->settings->commentswikibar->commentswikibar_active &&
+			$core->blog->settings->system->wiki_comments)
+		{
+			$supported_modes = new ArrayObject(array('post','pages','gal','galitem'));
+			$core->callBehavior('initCommentsWikibar',$supported_modes);
+			if (in_array($core->url->type,(array)$supported_modes)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	public static function publicHeadContent()
 	{
 		global $core;
 		
-		if ($core->blog->settings->commentswikibar->commentswikibar_active && $core->blog->settings->system->wiki_comments)
-		{
-			$supported_modes = new ArrayObject(array('post','pages','gal','galitem'));
-			$core->callBehavior('initCommentsWikibar',$supported_modes);
-
-			if (in_array($core->url->type,(array)$supported_modes))
-			{
-				$custom_css = $core->blog->settings->commentswikibar->commentswikibar_custom_css;		
-				if (!empty($custom_css)) {
-					if (strpos('/',$custom_css) === 0) {
-						$css = $custom_css;
-					}
-					else {
-						$css =
-							$core->blog->settings->system->themes_url."/".
-							$core->blog->settings->system->theme."/".
-							$custom_css;
-					}
+		if (self::canActivate()) {
+			$custom_css = $core->blog->settings->commentswikibar->commentswikibar_custom_css;		
+			if (!empty($custom_css)) {
+				if (strpos('/',$custom_css) === 0) {
+					$css = $custom_css;
 				}
 				else {
-					$css = html::stripHostURL($core->blog->getQmarkURL().'pf=commentsWikibar/jsToolBar.comments.css');
+					$css =
+						$core->blog->settings->system->themes_url."/".
+						$core->blog->settings->system->theme."/".
+						$custom_css;
 				}
-
-				$js = html::stripHostURL($core->blog->getQmarkURL().'pf=commentsWikibar/jsToolBar.comments.js');
-
-				echo 
-					'<style type="text/css" media="screen">@import url('.$css.');</style>'."\n".
-					'<script type="text/javascript" src="'.$js.'"></script>'."\n".
-					'<script type="text/javascript">'."\n".
-					"//<![CDATA[\n".
-					"addListener(window,'load',function() {\n".
-					"jsToolBar.prototype.base_url = '".html::escapeJS($core->blog->host)."'; \n".
-					"jsToolBar.prototype.legend_msg = '".html::escapeJS(__('You can use the following shortcuts to format your text.'))."'; \n".
-					"jsToolBar.prototype.elements.strong.title = '".html::escapeJS(__('Strong emphasis'))."'; \n".
-					"jsToolBar.prototype.elements.em.title = '".html::escapeJS(__('Emphasis'))."'; \n".
-					"jsToolBar.prototype.elements.ins.title = '".html::escapeJS(__('Inserted'))."'; \n".
-					"jsToolBar.prototype.elements.del.title = '".html::escapeJS(__('Deleted'))."'; \n".
-					"jsToolBar.prototype.elements.quote.title = '".html::escapeJS(__('Inline quote'))."'; \n".
-					"jsToolBar.prototype.elements.code.title = '".html::escapeJS(__('Code'))."'; \n".
-					"jsToolBar.prototype.elements.ul.title = '".html::escapeJS(__('Unordered list'))."'; \n".
-					"jsToolBar.prototype.elements.ol.title = '".html::escapeJS(__('Ordered list'))."'; \n".
-					"jsToolBar.prototype.elements.link.title = '".html::escapeJS(__('Link'))."'; \n".
-					"jsToolBar.prototype.elements.link.href_prompt = '".html::escapeJS(__('URL?'))."'; \n".
-					"jsToolBar.prototype.elements.link.hreflang_prompt = '".html::escapeJS(__('Language?'))."'; \n\n".
-					"if (document.getElementById) { \n".
-					"	if (document.getElementById('".html::escapeJS('c_content')."')) { \n".
-					"		var commentTb = new jsToolBar(document.getElementById('".html::escapeJS('c_content')."')); \n".
-					"		commentTb.draw(); \n".
-					"	}\n".
-					"}\n".
-					"});\n".
-					"\n//]]>\n".
-					"</script>\n";
 			}
+			else {
+				$css = html::stripHostURL($core->blog->getQmarkURL().'pf=commentsWikibar/wikibar.css');
+			}
+			echo '<link rel="stylesheet" type="text/css" media="screen" href="'.$css.'"/>';
+		}
+	}
+	
+	public static function publicFooterContent()
+	{
+		global $core;
+		
+		if (self::canActivate()) {
+			$js = html::stripHostURL($core->blog->getQmarkURL().'pf=commentsWikibar/wikibar.js');
+
+			echo
+			'<script type="text/javascript" src="'.$js.'"></script>'."\n".
+			'<script type="text/javascript">'."\n".
+			"//<![CDATA[\n".
+			"addListener(window,'load',function() {\n".
+			"jsToolBar.prototype.base_url = '".html::escapeJS($core->blog->host)."'; \n".
+			"jsToolBar.prototype.legend_msg = '".html::escapeJS(__('You can use the following shortcuts to format your text.'))."'; \n".
+			"jsToolBar.prototype.elements.strong.title = '".html::escapeJS(__('Strong emphasis'))."'; \n".
+			"jsToolBar.prototype.elements.em.title = '".html::escapeJS(__('Emphasis'))."'; \n".
+			"jsToolBar.prototype.elements.ins.title = '".html::escapeJS(__('Inserted'))."'; \n".
+			"jsToolBar.prototype.elements.del.title = '".html::escapeJS(__('Deleted'))."'; \n".
+			"jsToolBar.prototype.elements.quote.title = '".html::escapeJS(__('Inline quote'))."'; \n".
+			"jsToolBar.prototype.elements.code.title = '".html::escapeJS(__('Code'))."'; \n".
+			"jsToolBar.prototype.elements.ul.title = '".html::escapeJS(__('Unordered list'))."'; \n".
+			"jsToolBar.prototype.elements.ol.title = '".html::escapeJS(__('Ordered list'))."'; \n".
+			"jsToolBar.prototype.elements.link.title = '".html::escapeJS(__('Link'))."'; \n".
+			"jsToolBar.prototype.elements.link.href_prompt = '".html::escapeJS(__('URL?'))."'; \n".
+			"jsToolBar.prototype.elements.link.hreflang_prompt = '".html::escapeJS(__('Language?'))."'; \n\n".
+			"if (document.getElementById) { \n".
+			"	if (document.getElementById('".html::escapeJS('c_content')."')) { \n".
+			"		var commentTb = new jsToolBar(document.getElementById('".html::escapeJS('c_content')."')); \n".
+			"		commentTb.draw(); \n".
+			"	}\n".
+			"}\n".
+			"});\n".
+			"\n//]]>\n".
+			"</script>\n";
 		}
 	}
 }
